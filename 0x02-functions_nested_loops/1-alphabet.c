5c491b6b4c3fcd73002ff3f99d793f584a8e9a76
Dapsonwickie <ibigbamidapo@gmail.com>
2022-12-13 12:59:08 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - lower case
+ *
+ * Return 0
+ */
+void print_alphabet(void)
+{
+	char letter = 'a'';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
