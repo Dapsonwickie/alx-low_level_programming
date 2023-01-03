f426ae41113f87709843936e8078d00fd7405b39
Dapsonwickie <ibigbamidapo@gmail.com>
2023-01-03 07:47:51 -0800

    done

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strspn-gets the length of a prefix substring
+ * @s: string to be searched
+ * @accept: matching string
+ * Return: an unsigned integer indicating the length of matched string
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int status;
+
+
+	i = 0;
+	while (s[i])
+	{
+		j = 0;
+		status = 1;
+		while (accept[j])
+		{
+			if (s[i] == accept[j])
+			{
+				status = 0;
+				break;
+			}
+			j++;
+		}
+		if (status == 1)
+		{
+			break;
+		}
+		i++;
+	}
+	return (i);
+}
