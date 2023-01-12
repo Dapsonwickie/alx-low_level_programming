20b4e896120bb6318926f82d9b6c86408c1c2588
Dapsonwickie <117745476+Dapsonwickie@users.noreply.github.com>
2023-01-12 17:31:30 -0400

    Update 100-realloc.c
    Done

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,6 +3,7 @@
 /**
  * _realloc - reallocates a memory block using malloc and free
  * @ptr: pointer to the memory previsouly allocated by malloc
+ * @old_size: size of the allocated memory for ptr
  * @new_size: new size of the new memory block
  * Return: pointer to the newly allocated memory block
  */
